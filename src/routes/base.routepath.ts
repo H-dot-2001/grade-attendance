f9bda3fecc5ca6fcfb1b01d24501454ba3689c52
H-dot-2001 <gianlukagallarday6@gmail.com>
2021-09-28 19:12:13 +0200

    update

--- src/routes/base.routepath.ts
+++ src/routes/base.routepath.ts
@@ -1,4 +1,4 @@
-export const basePath = process.env.NEXT_PUBLIC_HOST || '';
+export const basePath = process.env.NEXT_PUBLIC_HOST || 'https://polite-tree-0a0c19210.azurestaticapps.net';
 
 export interface RouteList {
   [key: string]: string | ((...args) => string);
