91800301cd5e940b8eb0f855ff14aedaabf92857
GianLuka2001 <65915831+H-dot-2001@users.noreply.github.com>
2021-09-29 03:46:53 +0200

    Update base.routepath.ts

--- src/routes/base.routepath.ts
+++ src/routes/base.routepath.ts
@@ -1,4 +1,4 @@
-export const basePath = process.env.NEXT_PUBLIC_HOST || 'https://polite-tree-0a0c19210.azurestaticapps.net';
+export const basePath = process.env.NEXT_PUBLIC_HOST || 'https://mango-mushroom-0cd9e8110.azurestaticapps.net';
 
 export interface RouteList {
   [key: string]: string | ((...args) => string);
